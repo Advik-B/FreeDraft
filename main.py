1c76fbdd926d2b7ca7994432fa56b165b5b8c579
Advik <advik.b@gmail.com>
2023-10-17 20:32:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This Python file uses the following encoding: utf-8
-import sys
-from pathlib import Path
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt6.QtGui import QAction
+from PyQt6.uic import loadUi
+from path import Path
+from sys import argv
 
-from PySide6.QtGui import QGuiApplication
-from PySide6.QtQml import QQmlApplicationEngine
+
+class AppUI(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.init_ui()
+        self.register_ui()
+        self.show()
+
+    def init_ui(self):
+        loadUi(Path(__file__).parent / "form.ui", self)
+
+    def register_ui(self):
+        self.aboutAction = self.findChild(QAction, "actionAbout")
+        self.aboutActionQt = self.findChild(QAction, "actionAbout_Qt")
+
+        self.aboutAction.triggered.connect(self.about)
+        self.aboutActionQt.triggered.connect(self.about_qt)
+
+    def about(self):
+        QMessageBox.about(self, "About", "About")
+
+    def about_qt(self):
+        QMessageBox.aboutQt(self, "About Qt")
 
 
 if __name__ == "__main__":
-    app = QGuiApplication(sys.argv)
-    engine = QQmlApplicationEngine()
-    qml_file = Path(__file__).resolve().parent / "main.qml"
-    engine.load(qml_file)
-    if not engine.rootObjects():
-        sys.exit(-1)
-    sys.exit(app.exec())
+    app = QApplication(argv)
+    window = AppUI()
+    app.exec()
